3c3bb3e95c5b0890c0a9e4b2bf382ab3e759c431
Nate Kean <natekean65535@gmail.com>
2026-06-25 13:38:52 -0500

    Oh wait, weasel.gibberish is red

--- parrot/commands/text.py
+++ parrot/commands/text.py
@@ -170,8 +170,8 @@ class Text(commands.Cog):
 				sentence = irritate_text(sentence)
 				name = irritate_text(name)
 			case Text.ImitateMode.GIBBERISH:
-				sentence = weasel.gibberish(sentence)
-				name = weasel.gibberish(name)
+				sentence = await weasel.gibberish(sentence)
+				name = await weasel.gibberish(name)
 
 		# Prepare to send this sentence through a webhook.
 		# Discord lets you change the name and avatar of a webhook account much
